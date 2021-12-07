76fa6f68193a1e5d3242d70867da410c7a221942
Sehoon Park <60946925+Locke-Park@users.noreply.github.com>
2021-12-07 09:35:40 +0900

    반복문을 이용한 새로운 풀이이다.

--- Bronze5/8393.py
+++ Bronze5/8393.py
@@ -1,3 +1,19 @@
+#가우스 덧셈법을 이용한 풀이
+
 a = int(input())
 
 print(int(a * (a+1) / 2))
+
+#반복문과 리턴함수를 이용한 풀이
+
+n = int(input())
+
+def x(n):
+  total = 0 
+  for i in range(1, n+1):
+    total = total + i
+  return total
+
+print(x(n))
+
+
